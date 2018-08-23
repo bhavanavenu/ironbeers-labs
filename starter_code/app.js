2da72422c5dac7742e478837bd42b4858ac30546
Bhavana <bhavanavenugopal2@gmail.com>
2018-08-23 08:03:08 +0200

    partiallly done

--- starter_code/app.js
+++ starter_code/app.js
@@ -14,5 +14,17 @@ app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers=>{
+    res.render('beers',{beers});
+  })
+  .catch(error=>{
+    console.log('error');
+  })
+});
 
+app.get('/random-beer', (req, res, next) => {
+  res.render('random-beer');
+})
 app.listen(3000);
